5406385911f6daa7f8f3b696e4875e0052c1d381
Joaquin Franco <joafran0016@gmail.com>
2026-07-01 17:50:10 -0300

    chore: delete next static files and create og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export const metadata: Metadata = {
     type: "website",
     images: [
       {
-        url: "/avatar.png",
+        url: "/og-image.png",
         width: 1200,
         height: 630,
         alt: "Joaquin Franco",
